e278dba317ba4709344265068e050f3120a92e6d
awallace689 <awallace689@gmail.com>
2024-11-17 02:12:50 -0600

    cleanup

--- init.lua
+++ init.lua
@@ -612,18 +612,31 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+        --========================
+        --========= OLD ==========
+        --========================
+        -- -- clangd = {},
+        -- -- gopls = {},
+        -- -- pyright = {},
+        -- -- rust_analyzer = {},
+        -- -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        -- --
+        -- -- Some languages (like typescript) have entire language plugins that can be useful:
+        -- --    https://github.com/pmizio/typescript-tools.nvim
+        -- --
+        -- -- But for many setups, the LSP (`ts_ls`) will work just fine
+        -- -- ts_ls = {},
+        --========================
+        --========================
+        --========================
+        ts_ls = {},
+        html = {},
+        cssls = {},
+        jsonls = {},
+        markdownlint = {},
+        --========================
+        --========= NEW ==========
+        --========================
 
         lua_ls = {
           -- cmd = {...},
@@ -654,6 +667,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'prettierd',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -708,6 +722,8 @@ require('lazy').setup({
         lua = { 'stylua' },
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier' },
+        html = { 'prettierd', 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
@@ -788,9 +804,9 @@ require('lazy').setup({
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
-          --['<Tab>'] = cmp.mapping.select_next_item(),
-          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ['<CR>'] = cmp.mapping.confirm { select = true },
+          ['<Tab>'] = cmp.mapping.select_next_item(),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
@@ -1011,6 +1027,15 @@ require('lazy').setup({
 
 vim.keymap.set('i', 'jj', '<ESC>')
 
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+-- vim.opt.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
+
 --============ END KEYMAPS ==============
 
 --============ VIM CONFIG ===============
@@ -1029,22 +1054,15 @@ require('lspconfig').gleam.setup {}
 --============ END GLEAM ================
 
 --=========== TYPESCRIPT ================
--- added
+
+-- added LSPs (json, html, css, ts_ls) and prettierd
 
 --========== END TYPESCRIPT ==============
---
 
 --============ LIGHTLINE =================
 vim.g.lightline = { colorscheme = 'powerline' }
 --============ LIGHTLINE =================
+
 --=======================================
 --============= END CUSTOM ==============
 --=======================================
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
